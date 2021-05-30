45e5696f99e937982bff5814c7ce1d08d14e8d8a
Al-Jamil Suvo <aljamil.suvo@tigerit.com>
2021-05-30 17:09:37 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import json
+import logging
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
+from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
+import hashlib
+
+
+logger = logging.getLogger(__name__)
+
+
+class DemoExtension(Extension):
+    def __init__(self):
+        super(DemoExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+    def on_event(self, event, extension):
+        query = event.get_argument()
+        logger.info("preferences %s" % json.dumps(extension.preferences))
+        md5Hash=hashlib.md5(query)
+        items = [
+            ExtensionResultItem(
+                icon="images/icon.svg",
+                name="MD5 Hash = "+md5Hash,
+                description="Enter to copy the result\nAlt-enter to copy the expression",
+                on_enter=CopyToClipboardAction(md5Hash),
+                on_alt_enter=CopyToClipboardAction(md5Hash),
+            )
+        ]
+
+        return RenderResultListAction(items)
+
+
+if __name__ == "__main__":
+    DemoExtension().run()
